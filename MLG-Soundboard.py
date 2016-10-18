eb87d8e24c5f08fd49dc88a7be447bad5ed27926
tharris5109 <tharris5109@users.noreply.github.com>
2016-10-18 19:14:30 +0100

    Added the first revision, pre-Github development.
    Main .wav files added as well as rename of actual program to MLG-Soundboard.py to coincide with the Github repository name.

---
+++ MLG-Soundboard.py
@@ -0,0 +1,156 @@
+import winsound
+import os
+os.system("title mlg soundboard w/ montageparodies 420 m8")
+os.system("color f0")
+
+while True:
+    print("1.  Sad music")
+    print("2.  Sad music with airhorn")
+    print("3.  Airhorn sonata")
+    print("4.  Airhorn")
+    print("5.  Airhorn multiple")
+    print("6.  Damn son, where d'ya find this?")
+    print("7.  Damn son, you just hit with a wow effect!")
+    print("8.  Dankstorm")
+    print("9.  Dedotated wam")
+    print("10. Get noscoped")
+    print("11. Hitmarker")
+    print("12. Intervention 420")
+    print("13. Mom, get the camera out!")
+    print("14. No-one has ever done that...")
+    print("15. Noice with click")
+    print("16. Noice")
+    print("17. Noice repeated mlg m8")
+    print("18. Oh, baby a triple!")
+    print("19. OMG trickshot")
+    print("20. OHHHHHHHMYYYGOOOOOOODDDDDDDD")
+    print("21. Sanic")
+    print("22. ( ! )  Shots Fired!  ( ! )")
+    print("23. Skrillex")
+    print("24. Snoop Dogg")
+    print("25. Illuminati")
+    print("26. Tactical Nuke")
+    print("27. Whatcha say")
+    print("28. WOMBO COMBO")
+    print("29. WOW")
+    print("30. Coin")
+    print('31. "scrub"')
+    print('32. "lovely"')
+    print('33. "amazing"')
+    print("34. Before watching this I advise that your Mum is with you because...  when is the last time you seen her?  Seen her?  JOHN CENA!!! ")
+    print("35. Seen her...  JOHN CENA!!!")
+    print("36. JOHN CENA!!!")
+    print("37. What is going on here...?  BREAKFAST.")
+    print("38. BREAKFAST.")
+    print("39. DO IT")
+    print("40. DO IT!  JUST DO IT!!!")
+    print("41. This is great banter, it really is.")
+    print("42. Great banter.")
+    print("43. I have crippling depression.")
+    print("44. I have osteoperosis m8.")
+    print("45. Hey, that's pretty good!")
+    print("46. Watch me whip")
+    print("47. Whip, now watch me nae nae, now watch me whip, watch me nae nae")
+    print("48. Whip")
+    print('"exit" to close.')
+    print()
+    sound=input("Enter sound number:  ")
+    if sound == "1":
+        winsound.PlaySound("2sad4me.wav",winsound.SND_FILENAME)
+    elif sound == "2":
+        winsound.PlaySound("2sed4airhorn.wav",winsound.SND_FILENAME)
+    elif sound == "3":
+        winsound.PlaySound("airhorn sonata.wav",winsound.SND_FILENAME)
+    elif sound == "4":
+        winsound.PlaySound("airhorn.wav",winsound.SND_FILENAME)
+    elif sound == "5":
+        winsound.PlaySound("airhornmulti.wav",winsound.SND_FILENAME)
+    elif sound == "6":
+        winsound.PlaySound("damnsonwhere.wav",winsound.SND_FILENAME)
+    elif sound == "7":
+        winsound.PlaySound("damnsonwow.wav",winsound.SND_FILENAME)
+    elif sound == "8":
+        winsound.PlaySound("dankstorm.wav",winsound.SND_FILENAME)
+    elif sound == "9":
+        winsound.PlaySound("dwam.wav",winsound.SND_FILENAME)
+    elif sound == "10":
+        winsound.PlaySound("getnoscoped.wav",winsound.SND_FILENAME)
+    elif sound == "11":
+        winsound.PlaySound("hitmarker.wav",winsound.SND_FILENAME)
+    elif sound == "12":
+        winsound.PlaySound("intervention420.wav",winsound.SND_FILENAME)
+    elif sound == "13":
+        winsound.PlaySound("momgetthecamera.wav",winsound.SND_FILENAME)
+    elif sound == "14":
+        winsound.PlaySound("neverdonethat.wav",winsound.SND_FILENAME)
+    elif sound == "15":
+        winsound.PlaySound("noice.wav",winsound.SND_FILENAME)
+    elif sound == "16":
+        winsound.PlaySound("noice quick.wav",winsound.SND_FILENAME)
+    elif sound == "17":
+        winsound.PlaySound("noice quick overlap.wav",winsound.SND_FILENAME)
+    elif sound == "18":
+        winsound.PlaySound("babyatriple.wav",winsound.SND_FILENAME)
+    elif sound == "19":
+        winsound.PlaySound("omgtrickshot.wav",winsound.SND_FILENAME)
+    elif sound == "20":
+        winsound.PlaySound("omg.wav",winsound.SND_FILENAME)
+    elif sound == "21":
+        winsound.PlaySound("sanic.wav",winsound.SND_FILENAME)
+    elif sound == "22":
+        winsound.PlaySound("shots fired.wav",winsound.SND_FILENAME)
+    elif sound == "23":
+        winsound.PlaySound("skrillex scary.wav",winsound.SND_FILENAME)
+    elif sound == "24":
+        winsound.PlaySound("swe.wav",winsound.SND_FILENAME)
+    elif sound == "25":
+        winsound.PlaySound("Illuminati.wav",winsound.SND_FILENAME)
+    elif sound == "26":
+        winsound.PlaySound("tactical nuke.wav",winsound.SND_FILENAME)
+    elif sound == "27":
+        winsound.PlaySound("whatcha say.wav",winsound.SND_FILENAME)
+    elif sound == "28":
+        winsound.PlaySound("wombo combo.wav",winsound.SND_FILENAME)
+    elif sound == "29":
+        winsound.PlaySound("wow.wav",winsound.SND_FILENAME)
+    elif sound == "30":
+        winsound.PlaySound("coin.wav",winsound.SND_FILENAME)
+    elif sound == "31":
+        winsound.PlaySound("scrub.wav",winsound.SND_FILENAME)
+    elif sound == "32":
+        winsound.PlaySound("lovely.wav",winsound.SND_FILENAME)
+    elif sound == "33":
+        winsound.PlaySound("amazing.wav",winsound.SND_FILENAME)
+    elif sound == "34":
+        winsound.PlaySound("beforefull.wav",winsound.SND_FILENAME)
+    elif sound == "35":
+        winsound.PlaySound("seenherjohncena.wav",winsound.SND_FILENAME)
+    elif sound == "36":
+        winsound.PlaySound("johncena.wav",winsound.SND_FILENAME)
+    elif sound == "37":
+        winsound.PlaySound("breakfast.wav",winsound.SND_FILENAME)
+    elif sound == "38":
+        winsound.PlaySound("breakfastquick.wav",winsound.SND_FILENAME)
+    elif sound == "39":
+        winsound.PlaySound("doit.wav",winsound.SND_FILENAME)
+    elif sound == "40":
+        winsound.PlaySound("doitjustdoit.wav",winsound.SND_FILENAME)
+    elif sound == "41":
+        winsound.PlaySound("greatbanter.wav",winsound.SND_FILENAME)
+    elif sound == "42":
+        winsound.PlaySound("greatbanterquick.wav",winsound.SND_FILENAME)
+    elif sound == "43":
+        winsound.PlaySound("ihavedepression.wav",winsound.SND_FILENAME)
+    elif sound == "44":
+        winsound.PlaySound("osteoperosis.wav",winsound.SND_FILENAME)
+    elif sound == "45":
+        winsound.PlaySound("prettygood.wav",winsound.SND_FILENAME)
+    elif sound == "46":
+        winsound.PlaySound("watchmewhip.wav",winsound.SND_FILENAME)
+    elif sound == "47":
+        winsound.PlaySound("watchmewhipnaenae.wav",winsound.SND_FILENAME)
+    elif sound == "48":
+        winsound.PlaySound("whip.wav",winsound.SND_FILENAME)
+    elif sound == "exit":
+        exit()
+    os.system("cls")
